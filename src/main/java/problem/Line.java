e18ceb467dab90d516abb0c9c36982c2cd8f5cda
gladyshevns.22 <nigla05@mail.ru>
2021-04-15 10:56:26 +0300

    readme changed

--- src/main/java/problem/Line.java
+++ src/main/java/problem/Line.java
@@ -11,6 +11,8 @@ class Line
     public double y1;
     public double x2;
     public double y2;
+
+
     public Line() {
         this.A = 0;
         this.B = 0;
@@ -23,20 +25,31 @@ class Line
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
-        this.x1 = y2;
+        this.y2 = y2;
     }
     public void renderSect(GL2 gl, double size){
         gl.glPointSize((float) size);
         gl.glBegin(GL2.GL_LINES);
-        gl.glVertex2d(x1, x2);
-        gl.glVertex2d(y1, y2);
+        gl.glVertex2d(x1, y1);
+        gl.glVertex2d(x2, y2);
         gl.glEnd();
     }
     public void renderLine(GL2 gl, double size){
+        Vector2 v1 = new Vector2(x1,y1);
+        Vector2 v2 = new Vector2(x2,y2);
+        double w1 = x2-x1;
+        double w2 = y2-y1;
+
+        Vector2 v0 = new Vector2(w1/Math.sqrt(w1*w1+w2*w2), w2/Math.sqrt(w1*w1+w2*w2));
+
+        double k1 = x2 + 3*v0.x ;
+        double k2 = y2 + 3*v0.y ;
+        double k3 = x2-3*v0.x ;
+        double k4 = y2-3*v0.y ;
         gl.glPointSize((float) size);
         gl.glBegin(GL2.GL_LINES);
-        gl.glVertex2d(1, -C/B-A/B);
-        gl.glVertex2d(-C/A-B/A, 1);
+        gl.glVertex2d(k1, k2);
+        gl.glVertex2d(k3, k4);
         gl.glEnd();
     }
 }
--- src/main/java/problem/Problem.java
+++ src/main/java/problem/Problem.java
@@ -67,8 +67,8 @@ public class Problem {
      */
     public void solve() {
         double max = 0;
-        Point max_p1 = new Point(5,5);
-        Point max_p2 = new Point(5,5);
+        Point max_p1 = new Point(5, 5);
+        Point max_p2 = new Point(5, 5);
         // перебираем пары точек
         for (Point p : points) {
             for (Point p2 : points) {
@@ -76,119 +76,119 @@ public class Problem {
                 if (p != p2) {
                     // если координаты у них не совпадают
                     if (Math.abs(p.x - p2.x) >= 0.01 && Math.abs(p.y - p2.y) > 0.01) {
-                        Line l = new Line(rect.a1,rect.a2,rect.a3,rect.a4);
-                        Line l1 = new Line(rect.a5,rect.a6,rect.a5 + l.A,rect.a6 + l.B);
-                        Line l2 = new Line(rect.a5,rect.a6,rect.a5 + l1.A,rect.a6 + l1.B);
-                        Line lp1 = new Line(rect.a1,rect.a2,rect.a1 + l.A,rect.a2 + l.B);
-                        Line lp2 = new Line(rect.a3,rect.a4,rect.a3 + l.A,rect.a4 + l.B);
-                        Point o2 = new Point((lp1.B*l2.C-lp1.C*l2.B)/(lp1.A*l2.B-lp1.B*l2.A), (lp1.A*l2.C-lp1.C*l2.A)/(lp1.B*l2.A-lp1.A*l2.B));
-                        Point o1=new Point((lp2.B*l2.C-lp2.C*l2.B)/(lp2.A*l2.B-lp2.B*l2.A), (lp2.A*l2.C-lp2.C*l2.A)/(lp2.B*l2.A-lp2.A*l2.B));
-                        Line line = new Line(p.x,p.y,p2.x,p2.y);
-                        Point inter1 = new Point(2,2);
-                        Point inter2 = new Point(2,2);
-                        Point inter3 = new Point(2,2);
-                        Point inter4 = new Point(2,2);
+                        Line l = new Line(rect.a1, rect.a2, rect.a3, rect.a4);
+                        Line l1 = new Line(rect.a5, rect.a6, rect.a5 + l.A, rect.a6 + l.B);
+                        Line l2 = new Line(rect.a5, rect.a6, rect.a5 + l1.A, rect.a6 + l1.B);
+                        Line lp1 = new Line(rect.a1, rect.a2, rect.a1 + l.A, rect.a2 + l.B);
+                        Line lp2 = new Line(rect.a3, rect.a4, rect.a3 + l.A, rect.a4 + l.B);
+                        Point o2 = new Point((lp1.B * l2.C - lp1.C * l2.B) / (lp1.A * l2.B - lp1.B * l2.A), (lp1.A * l2.C - lp1.C * l2.A) / (lp1.B * l2.A - lp1.A * l2.B));
+                        Point o1 = new Point((lp2.B * l2.C - lp2.C * l2.B) / (lp2.A * l2.B - lp2.B * l2.A), (lp2.A * l2.C - lp2.C * l2.A) / (lp2.B * l2.A - lp2.A * l2.B));
+                        Line line = new Line(p.x, p.y, p2.x, p2.y);
+                        Point inter1 = new Point(2, 2);
+                        Point inter2 = new Point(2, 2);
+                        Point inter3 = new Point(2, 2);
+                        Point inter4 = new Point(2, 2);
                         int par1 = 0;
                         int par2 = 0;
                         int par3 = 0;
                         int par4 = 0;
                         boolean b;
-                        if(line.B!=l.B){
-                            b=Math.abs(l.A/l.B-line.A/line.B)<0.0001;
-                        }else{
-                            b=1==1;
+                        if (line.B != l.B) {
+                            b = Math.abs(l.A / l.B - line.A / line.B) < 0.0001;
+                        } else {
+                            b = 1 == 1;
                         }
-                        if(b==(1==0)){
-                            inter1.y=(line.A*l.C-line.C*l.A)/(line.B*l.A-line.A*l.B);
-                            inter1.x=(line.B*l.C-line.C*l.B)/(line.A*l.B-line.B*l.A);
+                        if (b == (1 == 0)) {
+                            inter1.y = (line.A * l.C - line.C * l.A) / (line.B * l.A - line.A * l.B);
+                            inter1.x = (line.B * l.C - line.C * l.B) / (line.A * l.B - line.B * l.A);
                         }
-                        if((Math.abs(inter1.x - rect.a1) + Math.abs(inter1.x - rect.a3) == Math.abs(rect.a3 - rect.a1))&&(Math.abs(inter1.y - rect.a2) + Math.abs(inter1.y - rect.a4) == Math.abs(rect.a2 - rect.a4))){
+                        if ((Math.abs(inter1.x - rect.a1) + Math.abs(inter1.x - rect.a3) == Math.abs(rect.a3 - rect.a1)) && (Math.abs(inter1.y - rect.a2) + Math.abs(inter1.y - rect.a4) == Math.abs(rect.a2 - rect.a4))) {
                             par1++;
                         }
-                        if(line.B!=lp2.B){
-                            b=Math.abs(lp2.A/lp2.B-line.A/line.B)<0.0001;
-                        }else{
-                            b=1==1;
+                        if (line.B != lp2.B) {
+                            b = Math.abs(lp2.A / lp2.B - line.A / line.B) < 0.0001;
+                        } else {
+                            b = 1 == 1;
                         }
-                        if(b==(1==0)){
-                            inter2.y=(line.A*lp2.C-line.C*lp2.A)/(line.B*lp2.A-line.A*lp2.B);
-                            inter2.x=(line.B*lp2.C-line.C*lp2.B)/(line.A*lp2.B-line.B*lp2.A);
+                        if (b == (1 == 0)) {
+                            inter2.y = (line.A * lp2.C - line.C * lp2.A) / (line.B * lp2.A - line.A * lp2.B);
+                            inter2.x = (line.B * lp2.C - line.C * lp2.B) / (line.A * lp2.B - line.B * lp2.A);
                         }
-                        if((Math.abs(inter2.x - o1.x) + Math.abs(inter2.x - rect.a3) == Math.abs(rect.a3 - o1.x))&&(Math.abs(inter2.y - rect.a2) + Math.abs(inter2.y - o1.y) == Math.abs(rect.a2 - o1.y))){
+                        if ((Math.abs(inter2.x - o1.x) + Math.abs(inter2.x - rect.a3) == Math.abs(rect.a3 - o1.x)) && (Math.abs(inter2.y - rect.a2) + Math.abs(inter2.y - o1.y) == Math.abs(rect.a2 - o1.y))) {
                             par2++;
                         }
-                        if(line.B!=l2.B){
-                            b=Math.abs(l2.A/l2.B-line.A/line.B)<0.0001;
-                        }else{
-                            b=1==1;
+                        if (line.B != l2.B) {
+                            b = Math.abs(l2.A / l2.B - line.A / line.B) < 0.0001;
+                        } else {
+                            b = 1 == 1;
                         }
-                        if(b==(1==0)){
-                            inter3.y=(line.A*l2.C-line.C*l2.A)/(line.B*l2.A-line.A*l2.B);
-                            inter3.x=(line.B*l2.C-line.C*l2.B)/(line.A*l2.B-line.B*l2.A);
+                        if (b == (1 == 0)) {
+                            inter3.y = (line.A * l2.C - line.C * l2.A) / (line.B * l2.A - line.A * l2.B);
+                            inter3.x = (line.B * l2.C - line.C * l2.B) / (line.A * l2.B - line.B * l2.A);
                         }
-                        if((Math.abs(inter3.x - o1.x) + Math.abs(inter3.x - o2.x) == Math.abs(o1.x - o2.x))&&(Math.abs(inter3.y - o1.y) + Math.abs(inter3.y - o2.y) == Math.abs(o1.y - o2.y))){
+                        if ((Math.abs(inter3.x - o1.x) + Math.abs(inter3.x - o2.x) == Math.abs(o1.x - o2.x)) && (Math.abs(inter3.y - o1.y) + Math.abs(inter3.y - o2.y) == Math.abs(o1.y - o2.y))) {
                             par3++;
                         }
-                        if(line.B!=lp1.B){
-                            b=Math.abs(lp1.A/lp1.B-line.A/line.B)<0.0001;
-                        }else{
-                            b=1==1;
+                        if (line.B != lp1.B) {
+                            b = Math.abs(lp1.A / lp1.B - line.A / line.B) < 0.0001;
+                        } else {
+                            b = 1 == 1;
                         }
-                        if(b==(1==0)){
-                            inter4.y=(line.A*lp1.C-line.C*lp1.A)/(line.B*lp1.A-line.A*lp1.B);
-                            inter4.x=(line.B*lp1.C-line.C*lp1.B)/(line.A*lp1.B-line.B*lp1.A);
+                        if (b == (1 == 0)) {
+                            inter4.y = (line.A * lp1.C - line.C * lp1.A) / (line.B * lp1.A - line.A * lp1.B);
+                            inter4.x = (line.B * lp1.C - line.C * lp1.B) / (line.A * lp1.B - line.B * lp1.A);
                         }
-                        if((Math.abs(inter4.x - rect.a1) + Math.abs(inter4.x - o2.x) == Math.abs(o2.x - rect.a1))&&(Math.abs(inter4.y - rect.a2) + Math.abs(inter4.y - o2.y) == Math.abs(rect.a2 - o2.y))){
+                        if ((Math.abs(inter4.x - rect.a1) + Math.abs(inter4.x - o2.x) == Math.abs(o2.x - rect.a1)) && (Math.abs(inter4.y - rect.a2) + Math.abs(inter4.y - o2.y) == Math.abs(rect.a2 - o2.y))) {
                             par4++;
                         }
-                        if((par1 != 0)&&(par2 != 0)){
-                            if((inter1.x-inter2.x)*(inter1.x-inter2.x)+(inter1.y-inter2.y)*(inter1.y-inter2.y)>max){
-                                max = (inter1.x-inter2.x)*(inter1.x-inter2.x)+(inter1.y-inter2.y)*(inter1.y-inter2.y);
+                        if ((par1 != 0) && (par2 != 0)) {
+                            if ((inter1.x - inter2.x) * (inter1.x - inter2.x) + (inter1.y - inter2.y) * (inter1.y - inter2.y) > max) {
+                                max = (inter1.x - inter2.x) * (inter1.x - inter2.x) + (inter1.y - inter2.y) * (inter1.y - inter2.y);
                                 max_p1.x = inter1.x;
                                 max_p1.y = inter1.y;
                                 max_p2.x = inter2.x;
                                 max_p2.y = inter2.y;
                             }
                         }
-                        if((par1 != 0)&&(par3 != 0)){
-                            if((inter1.x-inter3.x)*(inter1.x-inter3.x)+(inter1.y-inter3.y)*(inter1.y-inter3.y)>max){
-                                max = (inter1.x-inter3.x)*(inter1.x-inter3.x)+(inter1.y-inter3.y)*(inter1.y-inter3.y);
+                        if ((par1 != 0) && (par3 != 0)) {
+                            if ((inter1.x - inter3.x) * (inter1.x - inter3.x) + (inter1.y - inter3.y) * (inter1.y - inter3.y) > max) {
+                                max = (inter1.x - inter3.x) * (inter1.x - inter3.x) + (inter1.y - inter3.y) * (inter1.y - inter3.y);
                                 max_p1.x = inter1.x;
                                 max_p1.y = inter1.y;
                                 max_p2.x = inter3.x;
                                 max_p2.y = inter3.y;
                             }
                         }
-                        if((par1 != 0)&&(par4 != 0)){
-                            if((inter1.x-inter4.x)*(inter1.x-inter4.x)+(inter1.y-inter4.y)*(inter1.y-inter4.y)>max){
-                                max = (inter1.x-inter4.x)*(inter1.x-inter4.x)+(inter1.y-inter4.y)*(inter1.y-inter4.y);
+                        if ((par1 != 0) && (par4 != 0)) {
+                            if ((inter1.x - inter4.x) * (inter1.x - inter4.x) + (inter1.y - inter4.y) * (inter1.y - inter4.y) > max) {
+                                max = (inter1.x - inter4.x) * (inter1.x - inter4.x) + (inter1.y - inter4.y) * (inter1.y - inter4.y);
                                 max_p1.x = inter1.x;
                                 max_p1.y = inter1.y;
                                 max_p2.x = inter4.x;
                                 max_p2.y = inter4.y;
                             }
                         }
-                        if((par2 != 0)&&(par3 != 0)){
-                            if((inter2.x-inter3.x)*(inter2.x-inter3.x)+(inter2.y-inter3.y)*(inter2.y-inter3.y)>max){
-                                max = (inter2.x-inter3.x)*(inter2.x-inter3.x)+(inter2.y-inter3.y)*(inter2.y-inter3.y);
+                        if ((par2 != 0) && (par3 != 0)) {
+                            if ((inter2.x - inter3.x) * (inter2.x - inter3.x) + (inter2.y - inter3.y) * (inter2.y - inter3.y) > max) {
+                                max = (inter2.x - inter3.x) * (inter2.x - inter3.x) + (inter2.y - inter3.y) * (inter2.y - inter3.y);
                                 max_p1.x = inter2.x;
                                 max_p1.y = inter2.y;
                                 max_p2.x = inter3.x;
                                 max_p2.y = inter3.y;
                             }
                         }
-                        if((par2 != 0)&&(par4 != 0)){
-                            if((inter2.x-inter4.x)*(inter2.x-inter4.x)+(inter2.y-inter4.y)*(inter2.y-inter4.y)>max){
-                                max = (inter2.x-inter4.x)*(inter2.x-inter4.x)+(inter2.y-inter4.y)*(inter2.y-inter4.y);
+                        if ((par2 != 0) && (par4 != 0)) {
+                            if ((inter2.x - inter4.x) * (inter2.x - inter4.x) + (inter2.y - inter4.y) * (inter2.y - inter4.y) > max) {
+                                max = (inter2.x - inter4.x) * (inter2.x - inter4.x) + (inter2.y - inter4.y) * (inter2.y - inter4.y);
                                 max_p1.x = inter2.x;
                                 max_p1.y = inter2.y;
                                 max_p2.x = inter4.x;
                                 max_p2.y = inter4.y;
                             }
                         }
-                        if((par3 != 0)&&(par4 != 0)){
-                            if((inter4.x-inter3.x)*(inter4.x-inter3.x)+(inter4.y-inter3.y)*(inter4.y-inter3.y)>max){
-                                max = (inter4.x-inter3.x)*(inter4.x-inter3.x)+(inter4.y-inter3.y)*(inter4.y-inter3.y);
+                        if ((par3 != 0) && (par4 != 0)) {
+                            if ((inter4.x - inter3.x) * (inter4.x - inter3.x) + (inter4.y - inter3.y) * (inter4.y - inter3.y) > max) {
+                                max = (inter4.x - inter3.x) * (inter4.x - inter3.x) + (inter4.y - inter3.y) * (inter4.y - inter3.y);
                                 max_p1.x = inter4.x;
                                 max_p1.y = inter4.y;
                                 max_p2.x = inter3.x;
@@ -199,12 +199,18 @@ public class Problem {
                 }
             }
         }
-        if(max_p1.x==5){
+        if (max_p1.x == 5) {
             System.out.println("ERROR");
         } else {
-            System.out.printf("resA.x = %.2f, resA.y = %.2f"+
-                    "resB.x = %.2f,resB.y = %.2f" +
-                    "resLine.A = %.2f,resLine.B = %.2f,resLine.C = %.2f", max_p1.x, max_p1.y, max_p2.x, max_p2.y,max_p1.x, max_p1.y, max_p2.x, max_p2.y);
+            System.out.println(max_p1);
+            System.out.println(max_p2);
+            resA = new Point(max_p1.x, max_p1.y);
+            resB = new Point(max_p2.x, max_p2.y);
+
+            resLine = new Line(resA.x, resA.y, resB.x, resB.y);
+            System.out.printf("resA.x = %.2f, resA.y = %.2f\n", resA.x, resA.y);
+            System.out.printf("resB.x = %.2f, resB.y = %.2f\n", resB.x, resB.y);
+            System.out.printf("resLine.A = %.2f,resLine.B = %.2f,resLine.C = %.2f\n", resLine.A, resLine.B, resLine.C);
         }
     }
 
@@ -223,7 +229,7 @@ public class Problem {
             double x3 = sc.nextDouble();
             double y3 = sc.nextDouble();
             sc.nextLine();
-            rect = new Rect(x1,y1,x2,y2,x3,y3);
+            rect = new Rect(x1, y1, x2, y2, x3, y3);
             // пока в файле есть непрочитанные строки
             while (sc.hasNextLine()) {
                 double x = sc.nextDouble();
@@ -287,16 +293,22 @@ public class Problem {
      * @param gl переменная OpenGL для рисования
      */
     public void render(GL2 gl) {
+        gl.glLineWidth(1);
+        gl.glColor3d(1, 1, 1);
         for (Point point : points) {
             point.render(gl);
         }
 //        Rect.renderRect(gl, new Vector2(0,0), new Vector2(0.1,0.1), new Vector2(0,-0.3), 1==1);
         if (rect != null)
             rect.renderRect(gl);
+
         if (resLine != null) {
             resLine.renderLine(gl, 1);
+            gl.glColor3d(1, 0, 0);
             resA.render(gl);
             resB.render(gl);
+            gl.glLineWidth(5);
+            resLine.renderSect(gl, 5);
         }
     }
 
